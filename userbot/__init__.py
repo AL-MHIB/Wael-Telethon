00dc2decfedef509fe197f08efd9abe5b307d8b5
weall2022 <102628172+weall2022@users.noreply.github.com>
2022-03-30 00:54:25 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -7,7 +7,7 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 __version__ = "7.6"
 __license__ = "GNU Affero General Public License v3.0"
 __author__ = "<t.me/iqthon>"
-__copyright__ = "telethon AR (C) 2020 - 2021  " + __author__
+__copyright__ = "weall (C) 2020 - 2021  " + __author__
 iqthon.version = __version__
 iqthon.tgbot.version = __version__
 LOGS = logging.getLogger("IQTHON")
@@ -15,7 +15,7 @@ bot = iqthon
 StartTime = time.time()
 catversion = "7.6"
 if Config.UPSTREAM_REPO == "Telethon-Ar":
-    UPSTREAM_REPO_URL = "https://github.com/telethon-Arab/telethonNow"
+    UPSTREAM_REPO_URL = "https://github.com/weall2022/Wael-Telethon"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
